6d63d74b2349ccefed6eaba9b28e66297dd82338
AswinWebDev <aswinvinod0014@gmail.com>
2023-06-02 15:33:25 +0530

    updated checkbox

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import Card1 from "../assets/images/image2.png";
 import Card2 from "../assets/images/image3.png";
+import { useState } from "react";
 
 const HomePageContainer = styled.div`
   display: flex;
@@ -35,27 +36,59 @@ const CheckboxLabel = styled.label`
 `;
 
 const HomePage = () => {
+  const [isChecked, setIsChecked] = useState({
+    checkbox1: false,
+    checkbox2: false,
+    checkbox3: false,
+  });
+
+  const handleCheckboxChange = (event) => {
+    setIsChecked({
+      ...{
+        checkbox1: false,
+        checkbox2: false,
+        checkbox3: false,
+      },
+      [event.target.id]: event.target.checked,
+    });
+  };
+
   return (
     <HomePageContainer>
       <CardContainer>
         <Card src={Card2} alt="card1" />
         <CheckboxForm>
-          <CheckboxInput type="checkbox" id="checkbox" />
-          <CheckboxLabel>Layout Selected</CheckboxLabel>
+          <CheckboxInput
+            type="checkbox"
+            id="checkbox1"
+            checked={isChecked.checkbox1}
+            onChange={handleCheckboxChange}
+          />
+          <CheckboxLabel htmlFor="checkbox1">Layout Selected</CheckboxLabel>
         </CheckboxForm>
       </CardContainer>
       <CardContainer>
         <Card src={Card1} alt="card1" />
         <CheckboxForm>
-          <CheckboxInput type="checkbox" id="checkbox" />
-          <CheckboxLabel>Layout Selected</CheckboxLabel>
+          <CheckboxInput
+            type="checkbox"
+            id="checkbox2"
+            checked={isChecked.checkbox2}
+            onChange={handleCheckboxChange}
+          />
+          <CheckboxLabel htmlFor="checkbox2">Layout Selected</CheckboxLabel>
         </CheckboxForm>
       </CardContainer>
       <CardContainer>
         <Card src={Card2} alt="card1" />
         <CheckboxForm>
-          <CheckboxInput type="checkbox" id="checkbox" />
-          <CheckboxLabel>Layout Selected</CheckboxLabel>
+          <CheckboxInput
+            type="checkbox"
+            id="checkbox3"
+            checked={isChecked.checkbox3}
+            onChange={handleCheckboxChange}
+          />
+          <CheckboxLabel htmlFor="checkbox3">Layout Selected</CheckboxLabel>
         </CheckboxForm>
       </CardContainer>
     </HomePageContainer>
